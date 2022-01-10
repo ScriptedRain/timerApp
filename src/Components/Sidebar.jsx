cdb95cb74a2277090352b1bb7ae2c13e725c3d03
GhostReaper46 <83068695+GhostReaper46@users.noreply.github.com>
2022-01-10 17:41:23 +1100

    Update Sidebar.jsx

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -15,6 +15,11 @@ const Sidebar = () => {
       <SideBarIcon icon={<FiSettings size='28' />} text='Settings' to={'/'} />
     </div>
   )
+  return (
+    <div className='fixed top-0 left-0 h-screen w-16 flex flex-col bg-gray-900 text-white shadow-md'>
+      <SideBarIcon icon={<FiSettings size='28' />} text='Settings' to={'/'} />
+    </div>
+  )
 }
 
 const SideBarIcon = ({ icon, text = 'tooltip 💡', to }) => {
