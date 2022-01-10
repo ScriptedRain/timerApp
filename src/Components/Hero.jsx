544128134f4a0b52e6e66003e53148f0cffebcfc
GhostReaper46 <83068695+GhostReaper46@users.noreply.github.com>
2022-01-10 17:01:32 +1100

    Changes to the home page mianly

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -7,14 +7,15 @@ const Hero = () => {
         <div class='container px-6 mx-auto'>
           <div class='flex flex-col items-center py-6 lg:h-screen lg:flex-row'>
             <div class='lg:w-1/2'>
-              <h2 class='text-4xl font-semibold text-gray-100'>Brand</h2>
+              <h2 class='text-4xl font-semibold text-gray-100'>Dynamik</h2>
 
               <h3 class='text-2xl font-semibold text-gray-100'>
-                Hello <span class='text-blue-400'>Guest</span>
+                Welcome back <span class='text-blue-400'>user</span>
               </h3>
 
               <p class='mt-3 text-gray-100'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing.
+                Welcome to the ultimate productivity app, the only app you will ever need to stay productive. Now you don't have to have seperate apps for a calendar, timer and tasks to manage.
+               
               </p>
             </div>
 
