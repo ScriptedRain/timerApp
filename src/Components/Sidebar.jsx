ff30b516213f16331750b0c18570ee5c21c957f1
GhostReaper46 <83068695+GhostReaper46@users.noreply.github.com>
2022-01-10 18:24:55 +1100

    Making and connecting pages for sidebar icons :)

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -5,14 +5,14 @@ const Sidebar = () => {
   return (
     <div className='fixed top-0 left-0 h-screen w-16 flex flex-col bg-gray-900 text-white shadow-md'>
       <SideBarIcon icon={<AiFillHome size='28' />} text='Home' to={'/'} />
-      <SideBarIcon icon={<AiFillCalendar size='28' />} text='Calendar' />
-      <SideBarIcon icon={<AiFillClockCircle size='28' />} text='Stopwatch/Timer' />
+      <SideBarIcon icon={<AiFillCalendar size='28' />} text='Calendar' to={'Calendar'}/>
+      <SideBarIcon icon={<AiFillClockCircle size='28' />} text='Stopwatch/Timer' to={'Stopwatch and Timer'}/>
       <SideBarIcon
         icon={<AiOutlineTeam size='28' />}
         text='About Us'
         to={'/about'}
       />
-      <SideBarIcon icon={<FiSettings size='28' />} text='Settings' to={'/'} />
+      <SideBarIcon icon={<FiSettings size='28' />} text='Settings' to={'Settings'} />
     </div>
   )
   return (
