053fba6c569d2ba9f3a6f73614e3822519284e60
krishp058 <74632222+krishp058@users.noreply.github.com>
2022-01-10 18:11:32 +1100

    Fixed heading alignment

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const About = () => {
     return (
-        <div className=" bg-gray-900 2xl:container 2xl:mx-auto lg:py-16 lg:px-20 md:py-12 md:px-6 py-9 px-4">
+        <div className=" bg-gray-900 2xl:container 2xl:mx-auto lg:px-20 md:py-12 md:px-6 py-9 px-4">
             <div className="flex flex-col lg:flex-row justify-between gap-8">
-                <div className="w-full lg:w-5/12 flex flex-col justify-center">
+                <div className="w-full lg:w-5/12 flex flex-col justify-center pb-48">
                     <h1 className="text-3xl lg:text-4xl font-bold leading-9 text-white pb-4">About Us</h1>
                     <p className="font-normal text-base leading-6 text-gray-400 ">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum.In the first place we have granted to God, and by this our present charter confirmed for us and our heirs forever that the English Church shall be free, and shall have her rights entire, and her liberties inviolate; and we will that it be thus observed; which is apparent from</p>
                 </div>
@@ -14,7 +14,7 @@ const About = () => {
             </div>
 
             <div className="flex lg:flex-row flex-col justify-between gap-8 pt-12">
-                <div className="w-full lg:w-5/12 flex flex-col justify-center">
+                <div className="w-full lg:w-5/12 flex flex-col justify-center pt-12">
                     <h1 className="text-3xl lg:text-4xl font-bold leading-9 text-white pb-4">Our Story</h1>
                     <p className="font-normal text-base leading-6 text-gray-400 ">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum.In the first place we have granted to God, and by this our present charter confirmed for us and our heirs forever that the English Church shall be free, and shall have her rights entire, and her liberties inviolate; and we will that it be thus observed; which is apparent from</p>
                 </div>
