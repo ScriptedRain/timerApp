08b92e68fb0bf299b638df895c813cfe215a26da
ScriptedRain <71009876+ScriptedRain@users.noreply.github.com>
2022-01-14 11:49:59 +1100

    cleanedup for merge

--- src/Pages/Todo.jsx
+++ src/Pages/Todo.jsx
@@ -4,9 +4,7 @@ import Pattern from '../Components/Pattern'
 const Todo = () => {
   return (
     <div>
-      <Pattern>
-        <h1 className='text-2xl'></h1>
-      </Pattern>
+      <Pattern></Pattern>
     </div>
   )
 }
